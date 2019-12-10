3c91025e36d9496534c984725cd8bb9c66c081e1
Freakanoid123 <coconutcrash19@yahoo.com>
2019-12-10 16:05:33 -0600

    Updated Space.Types enum with currently-unknown Type found in MP7 and updated comment formatting.

--- MP6Editor/Space.cs
+++ MP6Editor/Space.cs
@@ -15,16 +15,16 @@ namespace MP6Editor
 {
     class Space
     {
-        //Position values are the distance from the origin point (center)
-        public float X; //X position
-        public float Y; //Y position
-        public float Z; //Z position
-        public byte[] crap = new byte[31]; //Not sure what this info means yet, but it's probably important
+        // Position values are the distance from the origin point (center).
+        public float X; // X position
+        public float Y; // Y position
+        public float Z; // Z position
+        public byte[] crap = new byte[31]; // Not sure what this info means yet, but it's probably important.
         public int type;
         public Texture2D texture;
-        public byte typePad; //Padding after the type byte; This probably isn't neccessary
-        public int linkAmount; //TODO: This variable is redundant (just do links.Count) and should be removed
-        public List<int> links = new List<int>(); //IDs of the spaces linked to
+        public byte typePad; // Padding after the type byte; This probably isn't neccessary.
+        public int linkAmount; // TODO: This variable is redundant (just do links.Count) and should be removed.
+        public List<int> links = new List<int>(); // IDs of the spaces linked to.
 
         public Space()
         {
@@ -49,19 +49,22 @@ namespace MP6Editor
             Blank = 0x00,
             Blue = 0x01,
             Red = 0x02,
-            Happening = 0x03, //No event occurs upon landing
+            Happening = 0x03,   // By default, No event will occur upon landing.
             Miracle = 0x04,
             Dueling = 0x05,
             DKBowser = 0x06,
             Star = 0x07,
             Orb = 0x08,
-            Shop = 0x09,      //Does not function on it's own
-            Ztar = 0x0A       //Does not funtion on it's own
+            Shop = 0x09,        // Does not function on it's own.
+            Ztar = 0x0A,        // Does not funtion on it's own.
+            Unknown1 = 0x0B,    // Used by MP7.
+            Unknown2 = 0x0C     // Just in case.
         }
 
-        //Some data in "crap[25]" correlates to flags that
-        //change how the player traverses the environment;
-        //Currently just for reference
+        /* Some data in "crap[25]" correlates to flags that
+         * change how the player traverses the environment;
+         * Currently just putting these here for reference.
+         */
         public enum TraversalFlags
         {
             NoUnique = 0x0000,
