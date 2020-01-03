cfa59977a769dd0b2d9d09f88c2163744690cc4f
Freakanoid123 <coconutcrash19@yahoo.com>
2020-01-03 15:54:45 -0600

    Added Camera.cs to handle camera logic.

---
+++ MP6Editor/Camera.cs
@@ -0,0 +1,17 @@
+﻿/*************************************
+ * Camera.cs   
+ *
+ * Handles the editor camera.
+ *************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP6Editor
+{
+    class Camera
+    {
+    }
+}
