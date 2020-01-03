976d5792f4ead4e4f1d794e8e527f45f8b24cf27
Freakanoid123 <coconutcrash19@yahoo.com>
2020-01-03 16:56:04 -0600

    Initial properties.

--- MP6Editor/Camera.cs
+++ MP6Editor/Camera.cs
@@ -8,10 +8,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace MP6Editor
 {
     class Camera
     {
+        public float Zoom { get; set; }
+        public Vector2 Location { get; set; }
+        public float Rotation { get; set; }
+
+        private Rectangle Bounds { get; set; }
+
+        private Matrix TransformMatrix
+        {
+            get {
+                return
+                    Matrix.CreateTranslation(new Vector3(-Location.X, -Location.Y, 0)) *
+                    Matrix.CreateRotationZ(Rotation) *
+                    Matrix.CreateScale(Zoom) *
+                    Matrix.CreateTranslation(new Vector3(Bounds.Width * 0.5f, Bounds.Height * 0.5f, 0));
+            }
+        }
+
+        public Camera(Viewport viewport)
+        {
+            Bounds = viewport.Bounds;
+        }
     }
 }
