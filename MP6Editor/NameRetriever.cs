21c58c742987979132b8edd75e6900ac5516834c
Freakanoid123 <coconutcrash19@yahoo.com>
2020-01-05 13:34:22 -0600

    Added summaries and renamed variables and methods to be more accurate to their actual function.

--- MP6Editor/NameRetriever.cs
+++ MP6Editor/NameRetriever.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*************************************
+ * NameRetriever.cs   
+ *
+ * Retrieves list of valid Space types
+ * or textures for passed MP version.
+ *************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,82 +21,87 @@ namespace MP6Editor
 
 
         /// <summary>
-        /// Get this type's matching texture.
+        /// Gets texture locations for passed game version.
         /// </summary>
-        /// <param name="type">Type of Space.</param>
-        /// <returns>Appropriate Texture for passed type; "X" Texture if unrecognized type.</returns>
+        /// <param name="version">Version of Mario Party.</param>
+        /// <returns>String list of each texture's location.</returns>
         static public List<string> GetTextureNames(int version)
         {
             switch (version)
             {
                 case 4: // Mario Party 4
-                    return MP4_GetSpaceTexture(Spaces_Directory);
+                    return MP4_GetSpaceTypes(Spaces_Directory);
                 case 5: // Mario Party 5
-                    return MP5_GetSpaceTexture(Spaces_Directory);
+                    return MP5_GetSpaceTypes(Spaces_Directory);
                 case 6: // Mario Party 6
-                    return MP6_GetSpaceTexture(Spaces_Directory);
+                    return MP6_GetSpaceTypes(Spaces_Directory);
                 case 7: // Mario Party 7
-                    return MP7_GetSpaceTexture(Spaces_Directory);
+                    return MP7_GetSpaceTypes(Spaces_Directory);
                 default:
-                    return MP7_GetSpaceTexture(Spaces_Directory);
+                    return MP7_GetSpaceTypes(Spaces_Directory);
             }
 
         }// end getTextureNames()
 
+        /// <summary>
+        /// Gets names of valid Space types for the passed Mario Party version.
+        /// </summary>
+        /// <param name="version">Version of Mario Party.</param>
+        /// <returns>String list of each valid type.</returns>
         static public List<string> GetSpaceNames(int version)
         {
             switch (version)
             {
                 case 4: // Mario Party 4
-                    return MP4_GetSpaceTexture("");
+                    return MP4_GetSpaceTypes("");
                 case 5: // Mario Party 5
-                    return MP5_GetSpaceTexture("");
+                    return MP5_GetSpaceTypes("");
                 case 6: // Mario Party 6
-                    return MP6_GetSpaceTexture("");
+                    return MP6_GetSpaceTypes("");
                 case 7: // Mario Party 7
-                    return MP7_GetSpaceTexture("");
+                    return MP7_GetSpaceTypes("");
                 default:
-                    return MP7_GetSpaceTexture("");
+                    return MP7_GetSpaceTypes("");
             }
 
-        }// end getTextureNames()
+        }// end getSpaceNames()
 
-        static private List<string> MP4_GetSpaceTexture(string directory)
+        static private List<string> MP4_GetSpaceTypes(string prepend)
         {
             List<string> names = new List<string>();
             foreach (string name in Enum.GetNames(typeof(Space.MP4_Types)))
             {
-                names.Add(directory + name);
+                names.Add(prepend + name);
             }
             return names;
         }
 
-        static private List<string> MP5_GetSpaceTexture(string directory)
+        static private List<string> MP5_GetSpaceTypes(string prepend)
         {
             List<string> names = new List<string>();
             foreach (string name in Enum.GetNames(typeof(Space.MP5_Types)))
             {
-                names.Add(directory + name);
+                names.Add(prepend + name);
             }
             return names;
         }
 
-        static private List<string> MP6_GetSpaceTexture(string directory)
+        static private List<string> MP6_GetSpaceTypes(string prepend)
         {
             List<string> names = new List<string>();
             foreach (string name in Enum.GetNames(typeof(Space.MP6_Types)))
             {
-                names.Add(directory + name);
+                names.Add(prepend + name);
             }
             return names;
         }
 
-        static private List<string> MP7_GetSpaceTexture(string directory)
+        static private List<string> MP7_GetSpaceTypes(string prepend)
         {
             List<string> names = new List<string>();
             foreach (string name in Enum.GetNames(typeof(Space.MP7_Types)))
             {
-                names.Add(directory + name);
+                names.Add(prepend + name);
             }
             return names;
         }
